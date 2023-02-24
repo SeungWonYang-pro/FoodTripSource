924e05143cd8fbfc504b0e79c53ad558128b2b86
SeungWonYang-pro <swjhjs2@gmail.com>
2023-02-24 16:43:11 +0900

    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,37 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import useGeolocation from "react-hook-geolocation";
 import styles from "../styles/Home.module.css";
 import foodTrip from "../images/foodTrip.png"
+
 import japanHome from "../images/japanHome.jpg"
 import americaHome from "../images/americaHome.jpg"
 import indiaHome from "../images/indiaHome.jpg"
-import vietnamHome from "../images/vietnamHome.jpg"
+import chinaHome from "../images/chinaHome.png"
+import vietnamHome from "../images/vietnamHome.png"
+import hongkongHome from "../images/hongkongHome.png"
+import italyHome from "../images/italyHome.png"
+import franceHome from "../images/franceHome.png"
+import thaiHome from "../images/thaiHome.png"
+import mexicoHome from "../images/mexicoHome.png"
+
+
 import airplane from "../images/airplane.png"
+
 import japan from "../images/japan.png"
 import china from "../images/china.png"
 import america from "../images/america.png"
 import india from "../images/india.png"
 import vietnam from "../images/vietnam.png"
+import hongkong from "../images/hongkong.png"
+import italy from "../images/italy.png"
+import france from "../images/france.png"
+import thai from "../images/thailand.png"
+import mexico from "../images/mexico.png"
+
+
+
 function Home() {
     const [country, setCountry] = useState("일본");
     const [city, setCity] = useState("Korea");
@@ -29,7 +48,7 @@ function Home() {
             setCimage(japan);
         }
         else if (country === "중국") {
-            setImage(japanHome);
+            setImage(chinaHome);
             setCimage(china);
         }
         else if (country === "미국") {
@@ -45,27 +64,53 @@ function Home() {
             setImage(vietnamHome);
             setCimage(vietnam);
         }
+        else if (country === "홍콩") {
+            setImage(hongkongHome);
+            setCimage(hongkong);
+        }
+        else if (country === "이탈리아") {
+            setImage(italyHome);
+            setCimage(italy);
+        }
+        else if (country === "프랑스") {
+            setImage(franceHome);
+            setCimage(france);
+
+        }
+        else if (country === "태국") {
+            setImage(thaiHome);
+            setCimage(thai);
+        }
+        else if (country === "멕시코") {
+            setImage(mexicoHome);
+            setCimage(mexico);
+        }
 
     }, [country])
-    const geolocation = useGeolocation({
-        enableHighAccuracy: true,
-        maximumAge: 15000,
-        timeout: 12000,
-    });
-
-    useEffect(() => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${geolocation.latitude}&lon=${geolocation.longitude}&appid=${process.env.REACT_APP_KEY}&units=metric`;
-        fetch(url).then(response => response.json()).then(data => {
-            console.log(data);
-            setCity(data.name);
-            if (data.name !== "Korea") {
-                setLoading(false);
-            }
-        });
-
-        console.log(url);
+    
+    const geo = navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
+    function onGeoOk(position){
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        
+        //kakao REST API에 get 요청을 보낸다.
+        //파라미터 x,y에 lon,lat을 넣어주고 API_KEY를 Authorization헤더에 넣어준다.
+        axios.get(`https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lon}&y=${lat}&input_coord=WGS84`
+        ,{headers:{Authorization:`KakaoAK ${process.env.REACT_APP_REST_API}`}}
+        )
+        .then(res=>{
+            console.log(res.data.documents[0].address.region_3depth_name)
+            setCity(res.data.documents[0].address.region_3depth_name);
+        }
+        ).catch(e=>console.log(e))
     }
-        , [geolocation.longitude, geolocation.latitude])
+    function onGeoError(){
+        alert("위치권한을 확인해주세요");
+    }
+
+
+
+    
 
     return (
         <div className={styles.container}>
@@ -84,6 +129,11 @@ function Home() {
                     <option value="미국">미국</option>
                     <option value="인도">인도</option>
                     <option value="베트남">베트남</option>
+                    <option value="홍콩" >홍콩</option>
+                    <option value="이탈리아">이탈리아</option>
+                    <option value="프랑스">프랑스</option>
+                    <option value="태국">태국</option>
+                    <option value="멕시코">멕시코</option>
                 </select>
                 <img className={styles.countryimage} src={cimage}/>
             </div>
--- src/routes/Map.js
+++ src/routes/Map.js
@@ -1,44 +1,87 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import useGeolocation from "react-hook-geolocation";
+import foodTrip from "../images/foodTrip.png"
+
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import styles from "../styles/Map.module.css";
 const { kakao } = window;
 function Map() {
-    var map;
-    const [markers, setMarkers] = useState([])
-    const { country } = useParams();
-
-    const geolocation = useGeolocation({
+    const [curCity, setCurCity] = useState("");
+    const geol = useGeolocation({
         enableHighAccuracy: true,
         maximumAge: 15000,
         timeout: 12000,
     });
-    const getCountry = () => {
+    const ge = navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+    function onGeoOk(position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
 
-        console.log(country);
-
+        //kakao REST API에 get 요청을 보낸다.
+        //파라미터 x,y에 lon,lat을 넣어주고 API_KEY를 Authorization헤더에 넣어준다.
+        axios.get(`https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lon}&y=${lat}&input_coord=WGS84`
+            , { headers: { Authorization: `KakaoAK ${process.env.REACT_APP_REST_API}` } }
+        )
+            .then(res => {
+                console.log(res.data.documents[0].address.region_3depth_name);
+                setCurCity(res.data.documents[0].address.region_3depth_name);
+            }
+            ).catch(e => console.log(e))
     }
+    function onGeoError() {
+        alert("위치권한을 확인해주세요");
+    }
+    var map;
+    const container = document.getElementById('map');
+    const { country } = useParams();
+    const ps = new kakao.maps.services.Places();
+    let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+    useEffect(() => {
+        getMap();
+        let curPos = { x: geol.longitude, y: geol.latitude, place_name: "현위치" };
+        displayMarker(curPos);
+        ps.keywordSearch(`${curCity} 초밥`, placesSearchCB);
+        ps.keywordSearch(`${curCity} 돈까스`, placesSearchCB);
+        ps.keywordSearch(`${curCity} 우동`, placesSearchCB);
+        ps.keywordSearch(`${curCity} 텐동`, placesSearchCB)
+        function placesSearchCB(data, status, pagination) {
+            if (status === kakao.maps.services.Status.OK) {
+                for (let i = 0; i < data.length; i++) {
+                    displayMarker(data[i]);
+                }
+            }
+        }
+
+        function displayMarker(place) {
+            let markers = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(place.y, place.x),
+            });
+
+            kakao.maps.event.addListener(markers, 'click', function () {
+                // 마커를 클릭하면 장소명이 인포윈도우에 표출
+                console.log(place);
+                infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+                infowindow.open(map, markers);
+            });
+        }
+    }, [curCity])
+
+
+
+
     const getMap = async () => {
-        const container = document.getElementById('map');
         const options = {
-            center: new kakao.maps.LatLng(geolocation.latitude, geolocation.longitude),
+            center: new kakao.maps.LatLng(geol.latitude, geol.longitude),
             level: 3
         };
         map = new kakao.maps.Map(container, options);
-        var markerPosition = new kakao.maps.LatLng(geolocation.latitude, geolocation.longitude);
 
-        var marker = new kakao.maps.Marker({
-            position: markerPosition
-        });
-        marker.setMap(map);
     }
-    useEffect(() => {
-        getMap();
-    }, [geolocation.latitude, geolocation.longitude]);
 
-    useEffect(() => {
-        getCountry();
-    })
+
     const { register, handleSubmit } = useForm();
     const onSubmit = (data) => {
         SearchReastaurant({ data })
@@ -51,17 +94,25 @@ function Map() {
     }
 
     return (
-        <div>
-            <div style={{ center: true }}>{country} 여행</div>
-            <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <label>원하는 것</label>
-                    <input {...register("name")} />
-                    <input type="submit" />
-                </form>
+        <div className={styles.container}>
+            <div className={styles.titleContainer}>
+                <img className={styles.homeName} src={foodTrip} />
             </div>
-            <div id='map' style={{ width: '1000px', height: '800px' }}></div>
-            
+            <div className={styles.map} id='map' style={{ width: '390px', height: '350px' }}></div>
+            <div >
+                <ul className={styles.bottomSheet}>
+                    <li>전체보기</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+
+                </ul>
+            </div>
         </div>
     );
 }
